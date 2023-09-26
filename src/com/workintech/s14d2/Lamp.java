9dae54d46fa7d5926fb9885001fc2ed5c657ad66
elifinesse <133097200+elifinesse@users.noreply.github.com>
2023-09-26 14:39:50 +0300

    lamp + bed done

---
+++ src/com/workintech/s14d2/Lamp.java
@@ -0,0 +1,29 @@
+package com.workintech.s14d2;
+
+public class Lamp {
+    private LampType style;
+    private boolean battery;
+    private int globRating;
+
+    public Lamp(LampType style, boolean battery, int globRating){
+        this.style = style;
+        this.battery = battery;
+        this.globRating = globRating;
+    }
+
+    public LampType getStyle() {
+        return style;
+    }
+
+    public boolean isBattery() {
+        return battery;
+    }
+
+    public int getGlobRating() {
+        return globRating;
+    }
+
+    public void turnOn(){
+        System.out.println("Lamp is being turned on.");
+    }
+}
